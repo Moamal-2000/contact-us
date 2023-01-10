045a4f465bc0ec7e4b8f98a0ea1e9567154fc78c
Moamal-2000 <moamalalaapro1@gmail.com>
2023-01-10 15:35:54 +0300

    html: simple changes in classes + added input download files css: effect on hover on drop files area + fix responsive text javascript: function to handle upload files process

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
-'use strict'
+"use strict";
+
+const uploadInput = document.querySelector(
+    '.drop-file-area input[type="file"]'
+  ),
+  nameFileElement = document.querySelector(".drop-file-area .name-file");
+
+function uploadFiles(e) {
+  if (e.target.value == "") {
+    return false;
+  }
+
+  let fileName = e.target.value.replace("C:\\fakepath\\", "");
+  if (fileName.includes("."))
+    fileName = fileName.slice(0, fileName.indexOf("."));
+
+  nameFileElement.textContent = fileName;
+  return true;
+}
+
+uploadInput.addEventListener("input", uploadFiles);
